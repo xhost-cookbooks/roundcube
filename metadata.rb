62f873341db990405a44435a3496a96c9fd069b6
Chris Fordham <chris.fordham@industrieit.com>
2014-12-04 08:55:09 +1100

    Add initial cookbook files generated from meez.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'roundcube'
+maintainer       'Chris Fordham'
+maintainer_email 'chris@fordham-nagy.id.au'
+license          'Apache 2.0'
+description      'Installs/Configures roundcube'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
